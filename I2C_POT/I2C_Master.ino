8dae6af766736c14a0a811e4b48e600ed32124eb
Yash Vaishampayan <87477812+YashMV@users.noreply.github.com>
2022-05-25 22:54:03 +0530

    Update I2C_Master.ino

--- I2C_POT/I2C_Master.ino
+++ I2C_POT/I2C_Master.ino
@@ -14,7 +14,7 @@ void setup()
 
 void loop()
 {
-  sensor=analogRead(A0); //gets he value of the potentiometer
+  sensor=analogRead(A0); //gets the value of the potentiometer
   val=map(sensor, 1, 1023, 0, 255); //changing to 8 bits
   Wire.beginTransmission(slave_addr); //I2C begin transmission
   Wire.write(val); //Send data to slave
