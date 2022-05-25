93729e5ecbc4e5f2b32abd7749eff1ce08decec3
Yash Vaishampayan <87477812+YashMV@users.noreply.github.com>
2022-05-25 23:52:29 +0530

    Update Sender.ino

--- UART_POT_MAP/Sender.ino
+++ UART_POT_MAP/Sender.ino
@@ -1,16 +1,16 @@
 //Sender's code
 
-int sensor;
+int sensor; //declaring the potentiometer reading variable
 
 void setup()
 {
-	pinMode(A0, INPUT);
-  Serial.begin(9600);
+	pinMode(A0, INPUT); //Input pin of potentiometer
+  Serial.begin(9600); //begins the serial monitor at 9600 Baud rate
 }
 
 void loop()
 {
-  sensor = analogRead(A0);
-  sensor=map(sensor, 0, 1023, 0, 255);
-  Serial.write(sensor);
+  sensor = analogRead(A0); //reading the value of potentiometer
+  sensor=map(sensor, 0, 1023, 0, 255); //mapping the sensor variable from 0-255, converting into 8 bits
+  Serial.write(sensor);  //Sending the data
 }
