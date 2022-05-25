f9c80a72dafeed6f3ac52048b3a6fa9c9c9bc8f6
Yash Vaishampayan <87477812+YashMV@users.noreply.github.com>
2022-05-25 22:51:10 +0530

    Update I2C_Master.ino

--- I2C_POT/I2C_Master.ino
+++ I2C_POT/I2C_Master.ino
@@ -2,20 +2,20 @@
 
 #include <Wire.h>
 #define slave_addr 2
-int sensor;
-byte val;
+int sensor; //Variable to read the incoming data
+byte val; //8 bit data to be sent
 
 void setup()
 {
-  Wire.begin();
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
+  Wire.begin(); //starts the I2C communication
+  pinMode(A0, INPUT); //Input pin to read potentiometer
+  Serial.begin(9600); //begins the serial monitor at 9600 Baud rate
 }
 
 void loop()
 {
-  sensor=analogRead(A0);
-  val=map(sensor, 1, 1023, 0, 255);
+  sensor=analogRead(A0); //gets he value of the potentiometer
+  val=map(sensor, 1, 1023, 0, 255); //changing to 8 bits
   Wire.beginTransmission(slave_addr);
   Wire.write(val);
   Wire.endTransmission();
