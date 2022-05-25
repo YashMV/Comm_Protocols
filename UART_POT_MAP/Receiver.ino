f7af6556872aa47657ff5b907a228eac10a395b0
Yash Vaishampayan <87477812+YashMV@users.noreply.github.com>
2022-05-25 23:56:04 +0530

    Update Receiver.ino

--- UART_POT_MAP/Receiver.ino
+++ UART_POT_MAP/Receiver.ino
@@ -1,14 +1,14 @@
 //Receiver's Code
 
-int result;
+int result; //declaring incoming data variable
 
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(9600); //begins the serial monitor at 9600 Baud rate
 }
 
 void loop()
 {
-  result=Serial.read();
-  Serial.println(result);
+  result=Serial.read(); //read the incoming data
+  Serial.println(result); //Print the result in Serial monitor
 }
